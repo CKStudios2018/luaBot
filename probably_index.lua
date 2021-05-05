e7fb67f441f9669c0e7f0b23689bb7045d190bbf
CKStudios2018 <75259904+CKStudios2018@users.noreply.github.com>
2021-05-05 13:53:05 +0200

    Update probably_index.lua

--- probably_index.lua
+++ probably_index.lua
@@ -1,1 +1,16 @@
-end
+local http = require("socket.http")
+ltn12 = require("ltn12")
+
+local payload = [[ {"username":"NAME","avatar_url":"","content":"MESSAGE"} ]]
+
+http.request
+{
+    url = "https://discordapp.com/api/webhooks/<redacted>",
+    method = "POST",
+    headers =
+    {
+        ["Content-Type"] = "application/json",
+        ["Content-Length"] = payload:len()
+    },
+    source = ltn12.source.string(payload),
+}
